0cb5d95f8a0aabeced8720c55a54a7dc7c4dffa3
Murad Lalayev <lalayevmurad@gmail.com>
2024-05-06 13:12:21 +0400

    my commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import HEADER from '../src/components/header.jsx'
 
 function App() {
 
   return (
     <>
-
+      <HEADER />
     </>
   )
 }
