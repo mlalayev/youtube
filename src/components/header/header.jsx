bd2e774aa7651e9ae6e7b5b5d488947c53a9f85e
Murad Lalayev <lalayevmurad@gmail.com>
2024-05-08 00:47:58 +0400

    my commit

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,14 +4,19 @@ import React, { useState } from 'react';
 import search from '../../assets/search.svg';
 import create from '../../assets/create.svg';
 import unnamed from '../../assets/unnamed.jpg';
+import history from '../../assets/history.svg';
 import Dropdown from '../dropdown/dropdown.jsx';
 import mainpage from '../../assets/mainpage.svg';
+import playlist from '../../assets/playlist.svg';
 import hamburger from '../../assets/hamburger.svg';
+import rightarrow from '../../assets/rightarrow.svg';
+import yourvideos from '../../assets/yourvideos.svg';
+import watchlater from '../../assets/watchlater.svg';
 import youtubelogo from '../../assets/youtubelogo.svg';
+import yourchannel from '../../assets/yourchannel.svg';
 import notifications from '../../assets/notifications.svg';
 import subscriptions from '../../assets/subscriptions.svg';
 import youtubeshorts from '../../assets/youtubeshorts.svg';
-import rightarrow from '../../assets/rightarrow.svg';
 
 
 function Header() {
@@ -26,7 +31,8 @@ function Header() {
         setInputFocused(false);
     };
 
-    return (
+
+        return (
         <header>
             <div className="headerleft">
                 <div className='hamhover' onClick={() => { setIsOpen(!isOpen) }}>
@@ -34,11 +40,53 @@ function Header() {
 
                     <div className={`dropdown-menu ${isOpen ? 'active' : 'inactive'}`}>
                         <ul className='dropdownul'>
-                            <Dropdown img={mainpage} text={'Main Page'} className='firstchild'/>
+                            <Dropdown img={mainpage} text={'Main Page'} className='firstchild' />
                             <Dropdown img={youtubeshorts} text={'Youtube Shorts'} />
                             <Dropdown img={subscriptions} text={'Subscriptions'} />
-                            <div className="hr"></div>
-                            <h1 className='you'>You <img src={rightarrow} /></h1>                        
+                            <div className="hr">.</div>
+                            <div className='dropdowndivv'>
+                                <h1 className='you'>You <img src={rightarrow} /></h1>
+                            </div>
+                            <Dropdown img={yourchannel} text={'Your Channel'} />
+                            <Dropdown img={history} text={'History'} />
+                            <Dropdown img={playlist} text={'Playlists'} />
+                            <Dropdown img={yourvideos} text={'Your Videos'} />
+                            <Dropdown img={watchlater} text={'Watch Later'} />
+                            <Dropdown img={subscriptions} text={'Subscriptions'} />
+                            <Dropdown img={subscriptions} text={'Subscriptions'} />
+                            <Dropdown img={subscriptions} text={'Subscriptions'} />
+                            <Dropdown img={subscriptions} text={'Subscriptions'} />
+                            <Dropdown img={subscriptions} text={'Subscriptions'} />
+                            <Dropdown img={subscriptions} text={'Subscriptions'} />
+                            <Dropdown img={subscriptions} text={'Subscriptions'} />
+                            <Dropdown img={subscriptions} text={'Subscriptions'} />
+                            <Dropdown img={subscriptions} text={'Subscriptions'} />
+                            <Dropdown img={subscriptions} text={'Subscriptions'} />
+                            <Dropdown img={subscriptions} text={'Subscriptions'} />
+                            <Dropdown img={subscriptions} text={'Subscriptions'} />
+                            <Dropdown img={subscriptions} text={'Subscriptions'} />
+                            <Dropdown img={subscriptions} text={'Subscriptions'} />
+                            <Dropdown img={subscriptions} text={'Subscriptions'} />
+                            <Dropdown img={subscriptions} text={'Subscriptions'} />
+                            <Dropdown img={subscriptions} text={'Subscriptions'} />
+                            <Dropdown img={subscriptions} text={'Subscriptions'} />
+                            <Dropdown img={subscriptions} text={'Subscriptions'} />
+                            <Dropdown img={subscriptions} text={'Subscriptions'} />
+                            <Dropdown img={subscriptions} text={'Subscriptions'} />
+                            <Dropdown img={subscriptions} text={'Subscriptions'} />
+                            <Dropdown img={subscriptions} text={'Subscriptions'} />
+                            <Dropdown img={subscriptions} text={'Subscriptions'} />
+                            <Dropdown img={subscriptions} text={'Subscriptions'} />
+                            <Dropdown img={subscriptions} text={'Subscriptions'} />
+                            <Dropdown img={subscriptions} text={'Subscriptions'} />
+                            <Dropdown img={subscriptions} text={'Subscriptions'} />
+                            <Dropdown img={subscriptions} text={'Subscriptions'} />
+                            <Dropdown img={subscriptions} text={'Subscriptions'} />
+                            <Dropdown img={subscriptions} text={'Subscriptions'} />
+                            <Dropdown img={subscriptions} text={'Subscriptions'} />
+                            <Dropdown img={subscriptions} text={'Subscriptions'} />
+                            <Dropdown img={subscriptions} text={'Subscriptions'} />
+
                         </ul>
                     </div>
                 </div>
@@ -76,7 +124,7 @@ function Header() {
                 <img src={unnamed} className='unnamed' />
             </div>
         </header>
-    );
+        );
 }
 
-export default Header;
+        export default Header;
