452dfaf72d4be2cd6ad5bcc60b2f6ae082417cdd
Murad Lalayev <lalayevmurad@gmail.com>
2024-05-08 12:48:20 +0400

    my commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import HEADER from '../src/components/header/header.jsx'
+import LEFTASIDE from '../src/components/leftaside/leftaside.jsx'
 
 function App() {
 
   return (
     <>
       <HEADER />
+      <LEFTASIDE />
     </>
   )
 }
---
+++ src/components/leftaside/leftaside.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../leftaside/leftaside.css'
+
+function leftaside() {
+  return (
+   <aside className='leftaside'>
+            <div className="elementsdiv">
+                <div className="elementfirst"></div>
+                <div className="elementsecond"></div>
+                <div className="elementthird"></div>
+                <div className="elementfourth"></div>
+            </div>
+   </aside>
+  )
+}
+
+export default leftaside
