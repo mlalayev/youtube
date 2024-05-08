e67fde14711e3606f771c35c58b7b473d660412f
Murad Lalayev <lalayevmurad@gmail.com>
2024-05-08 17:49:58 +0400

    my commit

--- src/components/mainpage/mainpage.jsx
+++ src/components/mainpage/mainpage.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import '../mainpage/mainpage.css'
 import VIDEOS from '../videos/videos.jsx'
 import thumbnailone from '../../assets/thumbnailone.jpg'
+import thumbnailtwo from '../../assets/tumbnailtwo.jpg'
+import thumbnailthree from '../../assets/thumbnailthree.jpg'
+import thumbnailfour from '../../assets/thumbnailfour.jpg'
 
 function mainpage() {
     return (
         <div className='aaa'>
             <VIDEOS img={thumbnailone} text={'15:54'} texth={'I Played WWE 2K24 vs CODY RHODES!'} textp={'BDE'} texttwo={'1.3 Mn views 3 month ago'}/>
-            <VIDEOS img={thumbnailone} text={'15:54'} texth={'I Played WWE 2K24 vs CODY RHODES!'} textp={'BDE'} texttwo={'1.3 Mn views 3 month ago'}/>
-            <VIDEOS img={thumbnailone} text={'15:54'} texth={'I Played WWE 2K24 vs CODY RHODES!'} textp={'BDE'} texttwo={'1.3 Mn views 3 month ago'}/>
-            <VIDEOS img={thumbnailone} text={'15:54'} texth={'I Played WWE 2K24 vs CODY RHODES!'} textp={'BDE'} texttwo={'1.3 Mn views 3 month ago'}/>
+            <VIDEOS img={thumbnailtwo} text={'21:10'} texth={'Yapay Zekadan Korkma Vakti Geldi mi?'} textp={'Evrim Ağacı'} texttwo={'1 Mn views 5 month ago'}/>
+            <VIDEOS img={thumbnailthree} text={'05:24'} texth={'Yeni AFFA - Futbolumuzda nələr dəyişəcək'} textp={'Futbolla Yaşamaq'} texttwo={'2,9 K views 1 month ago'}/>
+            <VIDEOS img={thumbnailfour} text={'04:02'} texth={'Post Malone ft. 21 Savage - rockstar (Official Music Video)'} textp={'Post Malone'} texttwo={'1.1B views  6 years ago'}/>
             <VIDEOS img={thumbnailone} text={'15:54'} texth={'I Played WWE 2K24 vs CODY RHODES!'} textp={'BDE'} texttwo={'1.3 Mn views 3 month ago'}/>
             <VIDEOS img={thumbnailone} text={'15:54'} texth={'I Played WWE 2K24 vs CODY RHODES!'} textp={'BDE'} texttwo={'1.3 Mn views 3 month ago'}/>
             <VIDEOS img={thumbnailone} text={'15:54'} texth={'I Played WWE 2K24 vs CODY RHODES!'} textp={'BDE'} texttwo={'1.3 Mn views 3 month ago'}/>
