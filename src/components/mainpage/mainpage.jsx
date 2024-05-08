f65087803fd82a56a91f724ca87449a56f2c060a
Murad Lalayev <lalayevmurad@gmail.com>
2024-05-08 18:26:57 +0400

    my commit

--- src/components/mainpage/mainpage.jsx
+++ src/components/mainpage/mainpage.jsx
@@ -1,30 +1,48 @@
-import React from 'react'
-import '../mainpage/mainpage.css'
-import VIDEOS from '../videos/videos.jsx'
-import thumbnailone from '../../assets/thumbnailone.jpg'
-import thumbnailtwo from '../../assets/tumbnailtwo.jpg'
-import thumbnailthree from '../../assets/thumbnailthree.jpg'
-import thumbnailfour from '../../assets/thumbnailfour.jpg'
+import React from 'react';
+import '../mainpage/mainpage.css';
+import VIDEOS from '../videos/videos.jsx';
+import thumbnailtwo from '../../assets/tumbnailtwo.jpg';
+import thumbnailone from '../../assets/thumbnailone.jpg';
+import thumbnailsix from '../../assets/thumbnailsix.jpg';
+import thumbnailten from '../../assets/thumbnailten.jpg';
+import thumbnailfour from '../../assets/thumbnailfour.jpg';
+import thumbnailfive from '../../assets/thumbnailfive.jpg';
+import thumbnailnine from '../../assets/thumbnailnine.jpg';
+import thumbnailthree from '../../assets/thumbnailthree.jpg';
+import thumbnailseven from '../../assets/thumbnailseven.jpg';
+import thumbnaileight from '../../assets/thumbnaileight.jpg';
+import thumbnaileleven from '../../assets/thumbnaileleven.jpg';
+import thumbnailtwelve from '../../assets/thumbnailtwelve.jpg';
+import thumbnailfifteen from '../../assets/thumbnailfifteen.jpg';
+import thumbnailthirteen from '../../assets/thumbnailthirteen.jpg';
+import thumbnailfourteen from '../../assets/thumbnailfourteen.jpg';
 
 function mainpage() {
     return (
         <div className='aaa'>
-            <VIDEOS img={thumbnailone} text={'15:54'} texth={'I Played WWE 2K24 vs CODY RHODES!'} textp={'BDE'} texttwo={'1.3 Mn views 3 month ago'}/>
-            <VIDEOS img={thumbnailtwo} text={'21:10'} texth={'Yapay Zekadan Korkma Vakti Geldi mi?'} textp={'Evrim Ağacı'} texttwo={'1 Mn views 5 month ago'}/>
-            <VIDEOS img={thumbnailthree} text={'05:24'} texth={'Yeni AFFA - Futbolumuzda nələr dəyişəcək'} textp={'Futbolla Yaşamaq'} texttwo={'2,9 K views 1 month ago'}/>
-            <VIDEOS img={thumbnailfour} text={'04:02'} texth={'Post Malone ft. 21 Savage - rockstar (Official Music Video)'} textp={'Post Malone'} texttwo={'1.1B views  6 years ago'}/>
-            <VIDEOS img={thumbnailone} text={'15:54'} texth={'I Played WWE 2K24 vs CODY RHODES!'} textp={'BDE'} texttwo={'1.3 Mn views 3 month ago'}/>
-            <VIDEOS img={thumbnailone} text={'15:54'} texth={'I Played WWE 2K24 vs CODY RHODES!'} textp={'BDE'} texttwo={'1.3 Mn views 3 month ago'}/>
-            <VIDEOS img={thumbnailone} text={'15:54'} texth={'I Played WWE 2K24 vs CODY RHODES!'} textp={'BDE'} texttwo={'1.3 Mn views 3 month ago'}/>
-            <VIDEOS img={thumbnailone} text={'15:54'} texth={'I Played WWE 2K24 vs CODY RHODES!'} textp={'BDE'} texttwo={'1.3 Mn views 3 month ago'}/>
-            <VIDEOS img={thumbnailone} text={'15:54'} texth={'I Played WWE 2K24 vs CODY RHODES!'} textp={'BDE'} texttwo={'1.3 Mn views 3 month ago'}/>
-            <VIDEOS img={thumbnailone} text={'15:54'} texth={'I Played WWE 2K24 vs CODY RHODES!'} textp={'BDE'} texttwo={'1.3 Mn views 3 month ago'}/>
-            <VIDEOS img={thumbnailone} text={'15:54'} texth={'I Played WWE 2K24 vs CODY RHODES!'} textp={'BDE'} texttwo={'1.3 Mn views 3 month ago'}/>
-            <VIDEOS img={thumbnailone} text={'15:54'} texth={'I Played WWE 2K24 vs CODY RHODES!'} textp={'BDE'} texttwo={'1.3 Mn views 3 month ago'}/>
-            <VIDEOS img={thumbnailone} text={'15:54'} texth={'I Played WWE 2K24 vs CODY RHODES!'} textp={'BDE'} texttwo={'1.3 Mn views 3 month ago'}/>
-            <VIDEOS img={thumbnailone} text={'15:54'} texth={'I Played WWE 2K24 vs CODY RHODES!'} textp={'BDE'} texttwo={'1.3 Mn views 3 month ago'}/>
-            <VIDEOS img={thumbnailone} text={'15:54'} texth={'I Played WWE 2K24 vs CODY RHODES!'} textp={'BDE'} texttwo={'1.3 Mn views 3 month ago'}/>
+            <VIDEOS img={thumbnailone} text={'15:54'} texth={'I Played WWE 2K24 vs CODY RHODES!'} textp={'BDE'} texttwo={'1.3 Mn views 3 month ago'} />
+            <VIDEOS img={thumbnailtwo} text={'21:10'} texth={'Yapay Zekadan Korkma Vakti Geldi mi?'} textp={'Evrim Ağacı'} texttwo={'1 Mn views 5 month ago'} />
+            <VIDEOS img={thumbnailthree} text={'05:24'} texth={'Yeni AFFA - Futbolumuzda nələr dəyişəcək'} textp={'Futbolla Yaşamaq'} texttwo={'2,9 K views 1 month ago'} />
+            <VIDEOS img={thumbnailfour} text={'04:02'} texth={'Post Malone ft. 21 Savage - rockstar (Official Music Video)'} textp={'Post Malone'} texttwo={'1.1B views  6 years ago'} />
+            <VIDEOS img={thumbnailfive} text={'09:06'} texth={'Famous Players Destroyed by Cristiano Ronaldo'} textp={'RAN7HD'} texttwo={'339K views  2 weeks ago '} />
+            <VIDEOS img={thumbnailsix} text={'10:42'} texth={'Cristiano Ronaldo 40 Iconic Goals'} textp={'Ronnie7M'} texttwo={'1.7M views  1 year ago'} />
+            <VIDEOS img={thumbnailseven} text={'12:23'} texth={'PSJ 0:1 Borussiya Dortmund | UEFA Çempionlar Liqası, yarımfinal | İCMAL'} textp={'CBC Sport'} texttwo={'81K views  15 hours ago'} />
+            <VIDEOS img={thumbnaileight} text={'13:54'} texth={'Real Madrid 3:3 Mançester Siti | UEFA Çempionlar Liqası, 1/4 final | İCMAL'} textp={'CBC Sport'} texttwo={'190K views  4 weeks ago'} />
+            <VIDEOS img={thumbnailnine} text={'03:03'} texth={'Vini Jr., Bellingham & Real Madrid receive new BMW cars!'} textp={'Real Madrid'} texttwo={'3.5M views  6 months ago '} />
+            <VIDEOS img={thumbnailten} text={'05:06'} texth={'Bakıdakı yeni metro stansiyasından EKSKLÜZİV GÖRÜNTÜLƏR: ilk dəfə AzTV-də'} textp={'AZTV Xəbər'} texttwo={'90K views  3 weeks ago '} />
+            <VIDEOS img={thumbnaileleven} text={'07:41'} texth={'Bu kənddə işıq, yol, qaz, telefon yoxdur'} textp={'İCTİMAİ TV'} texttwo={'312K views  1 year ago'} />
+            <VIDEOS img={thumbnailtwelve} text={'16:46'} texth={'Kazan Cutlets With Vegetables: Cooking on a Wood Fire'} textp={'Country Life Vlog'} texttwo={'3.5M views  4 months ago'} />
+            <VIDEOS img={thumbnailthirteen} text={'21:24'} texth={'Delicious Homemade Chicken Burger Recipe: Crispy, Juicy, and Irresistible!'} textp={'Country Life Vlog'} texttwo={'3.4M views  5 months ago'} />
+            <VIDEOS img={thumbnailfourteen} text={'32:51'} texth={"Dominik'te Ronaldinho Rüzgarı Esti | Survivor All Star 2024 40. Bölüm"} textp={'Survivor Türkiye'} texttwo={'1.9M views  2 months ago'} />
+            <VIDEOS img={thumbnailone} text={'15:54'} texth={'I Played WWE 2K24 vs CODY RHODES!'} textp={'BDE'} texttwo={'1.3 Mn views 3 month ago'} />
+            <VIDEOS img={thumbnailnine} text={'03:03'} texth={'Vini Jr., Bellingham & Real Madrid receive new BMW cars!'} textp={'Real Madrid'} texttwo={'3.5M views  6 months ago '} />
+            <VIDEOS img={thumbnailfour} text={'04:02'} texth={'Post Malone ft. 21 Savage - rockstar (Official Music Video)'} textp={'Post Malone'} texttwo={'1.1B views  6 years ago'} />
+            <VIDEOS img={thumbnaileleven} text={'07:41'} texth={'Bu kənddə işıq, yol, qaz, telefon yoxdur'} textp={'İCTİMAİ TV'} texttwo={'312K views  1 year ago'} />
+            <VIDEOS img={thumbnailtwo} text={'21:10'} texth={'Yapay Zekadan Korkma Vakti Geldi mi?'} textp={'Evrim Ağacı'} texttwo={'1 Mn views 5 month ago'} />
+            <VIDEOS img={thumbnailfive} text={'09:06'} texth={'Famous Players Destroyed by Cristiano Ronaldo'} textp={'RAN7HD'} texttwo={'339K views  2 weeks ago '} />
+            <VIDEOS img={thumbnailfifteen} text={'19:30'} texth={"Acun Ilıcalı, Survivor'daki Büyük Değişikliği Açıkladı! | Survivor All Star 2024 106. Bölüm"} textp={'Survivor Türkiye'} texttwo={'463,930 views  16 hours ago'} />
         </div>
+        
     )
 }
 
