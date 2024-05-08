7307c358e89cd7ceb54fb9c52dc752029676db7b
Murad Lalayev <lalayevmurad@gmail.com>
2024-05-08 12:57:35 +0400

    myy commit

--- src/components/leftaside/leftaside.jsx
+++ src/components/leftaside/leftaside.jsx
@@ -1,17 +1,37 @@
 import React from 'react'
 import '../leftaside/leftaside.css'
+import you from '../../assets/you.svg'
+import mainpage from '../../assets/mainpage.svg'
+import youtubeshorts from '../../assets/youtubeshorts.svg'
+import subscriptions from '../../assets/subscriptions.svg'
+
 
 function leftaside() {
-  return (
-   <aside className='leftaside'>
+    return (
+        <aside className='leftaside'>
             <div className="elementsdiv">
-                <div className="elementfirst"></div>
-                <div className="elementsecond"></div>
-                <div className="elementthird"></div>
-                <div className="elementfourth"></div>
+                <div className="elementfirst">
+                    <img src={mainpage} />
+                    <p>Main Page</p>
+                </div>
+
+                <div className="elementsecond">
+                    <img src={youtubeshorts} />
+                    <p>Shorts</p>
+                </div>
+
+                <div className="elementthird">
+                    <img src={subscriptions} />
+                    <p>Subscriptions</p>
+                </div>
+
+                <div className="elementfourth">
+                    <img src={you} />
+                    <p>You</p>
+                </div>
             </div>
-   </aside>
-  )
+        </aside>
+    )
 }
 
 export default leftaside
